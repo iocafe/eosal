27d6acc8b038952c637a5ded5b8cdf76544592cb
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-10-29 16:43:58 -0400

    xx

--- extensions/filesys/windows/osal_file.c
+++ extensions/filesys/windows/osal_file.c
@@ -308,7 +308,7 @@ osalStatus osal_file_flush(
 */
 osalStatus osal_file_write(
 	osalStream stream,
-	const os_uchar *buf,
+    const os_char *buf,
 	os_memsz n,
 	os_memsz *n_written,
 	os_int flags)
@@ -372,7 +372,7 @@ getout:
 */
 osalStatus osal_file_read(
 	osalStream stream,
-	os_uchar *buf,
+    os_char *buf,
 	os_memsz n,
 	os_memsz *n_read,
 	os_int flags)
