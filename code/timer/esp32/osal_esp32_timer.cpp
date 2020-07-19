94a617eb8281fcc340f5a30c8a582625190b5fcd
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-07-19 02:46:55 -0400

    xx

--- code/timer/esp32/osal_esp32_timer.cpp
+++ code/timer/esp32/osal_esp32_timer.cpp
@@ -18,27 +18,10 @@
 ****************************************************************************************************
 */
 #include "eosal.h"
-// #include "esp_timer/include/esp_timer.h"
 #include "esp_timer.h"
 
-#include "Arduino.h"
+// #include "Arduino.h"
 
-#if 0
-// Writing directly to  watch dog timers
-
-#include "soc/timer_group_struct.h"
-#include "soc/timer_group_reg.h"
-void feedTheDog(){
-  // feed dog 0
-  TIMERG0.wdt_wprotect=TIMG_WDT_WKEY_VALUE; // write enable
-  TIMERG0.wdt_feed=1;                       // feed dog
-  TIMERG0.wdt_wprotect=0;                   // write protect
-  // feed dog 1
-  TIMERG1.wdt_wprotect=TIMG_WDT_WKEY_VALUE; // write enable
-  TIMERG1.wdt_feed=1;                       // feed dog
-  TIMERG1.wdt_wprotect=0;                   // write protect
-}
-#endif
 
 /**
 ****************************************************************************************************
@@ -78,11 +61,7 @@ void osal_timer_initialize(
 void OS_ISR_FUNC_ATTR os_get_timer(
     os_timer *t)
 {
-#ifdef ESP_PLATFORM
     *t = (os_timer)esp_timer_get_time();
-#else
-    *t = (os_timer)millis();
-#endif
 }
 
 	
@@ -107,15 +86,7 @@ os_boolean OS_ISR_FUNC_ATTR os_has_elapsed(
     os_timer *start_t,
     os_int period_ms)
 {
-#ifdef ESP_PLATFORM
     return (os_boolean)(esp_timer_get_time() >= *start_t + 1000 * (os_timer)period_ms);
-#else
-    os_uint diff;
-
-    if (period_ms < 0) return OS_TRUE;
-    diff = (os_uint)millis() - (os_uint)*start_t;
-    return (os_boolean)((os_int)diff > period_ms);
-#endif
 }
 
 
@@ -142,18 +113,7 @@ os_boolean OS_ISR_FUNC_ATTR os_has_elapsed_since(
     os_timer *now_t,
     os_int period_ms)
 {
-#ifdef ESP_PLATFORM
     return (os_boolean)(*now_t >= *start_t + 1000 * (os_timer)period_ms);
-#else
-    os_uint diff;
-
-    if (period_ms < 0) return OS_TRUE;
-    diff = (os_uint)*now_t - (os_uint)*start_t;
-
-    /* Important, do signed compare: without this iocom, etc. may fail!
-     */
-    return (os_boolean)((os_int)diff > period_ms);
-#endif
 }
 
 
@@ -177,14 +137,7 @@ os_int OS_ISR_FUNC_ATTR os_get_ms_elapsed(
     os_timer *start_t,
     os_timer *now_t)
 {
-#ifdef ESP_PLATFORM
-    return (os_int)(*now_t - *start_t) / 1000;
-#else
-    os_uint diff;
-
-    diff = (os_uint)*now_t - (os_uint)*start_t;
-    return (os_int)diff;
-#endif
+    return (os_int)((*now_t - *start_t) / 1000);
 }
 
 
@@ -213,7 +166,6 @@ os_boolean OS_ISR_FUNC_ATTR os_timer_hit(
     os_timer *now_t,
     os_int period_ms)
 {
-#ifdef ESP_PLATFORM
     os_int64 diff, n, m, period_us;
 
     if (period_ms <= 0) return OS_TRUE;
@@ -239,32 +191,5 @@ os_boolean OS_ISR_FUNC_ATTR os_timer_hit(
 
     *memorized_t = m;
     return OS_TRUE;
-#else
-    os_uint diff, u, m, n;
-
-    if (period_ms <= 0) return OS_TRUE;
-    u = (os_uint)*now_t;
-    m = (os_uint)*memorized_t;
-    diff = u - m;
-
-    /* Important, do signed compare: without this iocom, etc. may fail.
-     */
-    if ((os_int)diff < period_ms) return OS_FALSE;
-
-    /* If we have a skew more than a period, skip hit times.
-     */
-    if (diff >= (os_uint)2*period_ms)
-    {
-        n = diff / period_ms;
-        m += n * period_ms;
-    }
-    else
-    {
-        m += (os_uint)period_ms;
-    }
-
-    *memorized_t = m;
-    return OS_TRUE;
-#endif
 }
 
