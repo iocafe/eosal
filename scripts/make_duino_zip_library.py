97b3e2b3be7950d5264856304b68282fa8b1cefc
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2021-01-25 13:52:17 -0500

    Making arduino zip libs

--- scripts/make_duino_zip_library.py
+++ scripts/make_duino_zip_library.py
@@ -1,80 +1,80 @@
 #!/usr/bin/env python3
 # make_duino_zip_library.py 25.1.2021/pekka
 # Create a zip library useful which can be installed by Arduino IDE.
-from os import listdir, makedirs
-from os.path import isfile, isdir, join, splitext, exists
-from shutil import copyfile
+# from os import listdir, makedirs
+from os import makedirs
+# from os.path import isfile, isdir, join, splitext, exists
+from os.path import exists
+# from shutil import copyfile
 import sys
+from shutil import make_archive
+from platform import system
+
 
 def mymakedir(targetdir):
     if not exists(targetdir):
         makedirs(targetdir)
 
-def copy_level_2(sourcedir,roottargetdir,targetdir):
-    files = listdir(sourcedir)
+def read_version_txt(version_path):
+    file_content = 'XXXXXX-XXXX'
 
-    # Copy header files
-    for f in files:
-        p = join(sourcedir, f)
-        if isfile(p):
-            e = splitext(p)[1]
-            if e == '.h':
-                mymakedir(targetdir)
-                t = join(targetdir, f)
-                copyfile(p, t)
-            if e == '.c' or e == '.cpp':
-                t = join(roottargetdir, f)
-                copyfile(p, t)
+    try:
+        with open(version_path + "/eosal_version.txt", mode='r') as file: # b is important -> binary
+            file_content = file.read()
+    except:
+        file_content = None
+        pass            
 
-def copy_info(f,sourcedir,targetdir):
-    infodir = sourcedir + '/build/arduino-library'
-    p = join(infodir, f)
-    t = join(targetdir, f)
-    if exists(p):
-        copyfile(p, t)
+    return file_content
 
-def copy_level_1(sourcedir,targetdir):
-    mymakedir(targetdir)
-    files = listdir(sourcedir)
+def zip_ip(libname, indir, outdir):
+    if system() == 'Windows':
+        if not indir.startswith("c:"):
+            indir = 'c:' + indir
+        if not outdir.startswith("c:"):
+            outdir = 'c:' + outdir
+        coderoot = 'c:/coderoot'
+    else:
+        coderoot = '/coderoot'
 
-    # Copy header files
-    for f in files:
-        p = join(sourcedir, f)
-        if isfile(p):
-            e = splitext(p)[1]
-            if e == '.h':
-                t = join(targetdir, f)
-                copyfile(p, t)
+    version_path = coderoot + '/eosal'
+    version = read_version_txt(version_path)
+    if version == None:
+        outpath = outdir + "/" + libname
+    else:
+        outpath = outdir + "/" + version + "-" + libname
 
-    # Copy code and extensions folders
-    copy_level_2(sourcedir + '/code', targetdir, targetdir + '/code')
-    copy_level_2(sourcedir + '/extensions/dynamicio', targetdir, targetdir + '/extensions/dynamicio')
-
-    # Copy informative arduino files
-    copy_info('library.json', sourcedir, targetdir)
-    copy_info('library.properties', sourcedir, targetdir)
-
-import shutil
-shutil.make_archive(output_filename, 'zip', dir_name)
+    make_archive(outpath, 'zip', indir)
 
 def mymain():
-    outdir = "/coderoot/lib/arduino-platformio/iocom"
-    expectplatform = True
+    indir = "/coderoot/lib/arduino/eosal"
+    outdir = "/tmp"
+    libname = "unknown"
+    expectoutdir  = False
+    expectindir  = False
     n = len(sys.argv)
     for i in range(1, n):
         if sys.argv[i][0] == "-":
             if sys.argv[i][1] == "o":
-                expectplatform = False
+                expectoutdir = True
+            if sys.argv[i][1] == "i":
+                expectindir = True
 
         else:
-            if not expectplatform:
-                outdir = sys.argv[i];
+            if  expectoutdir:
+                outdir = sys.argv[i]
+            elif  expectindir:
+                indir = sys.argv[i]
+            else:
+                libname = sys.argv[i]
 
-            expectplatform = True
+            expectoutdir = False
+            expectindir = False
 
-    copy_level_1("/coderoot/iocom", outdir)
+    mymakedir(outdir)
+    zip_ip(libname, indir, outdir)
 
-# Usage make_duino_zip_library.py -o /coderoot/lib/esp32/iocom
+# Usage make_duino_zip_library.py eosal -i /coderoot/lib/arduino/eosal -o /coderoot/lib/arduino-zips
 mymain()
 
 
