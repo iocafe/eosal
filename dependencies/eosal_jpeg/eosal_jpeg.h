ef3d980ff117ba7d88afdea9dbd6a4fb535afba3
mint pekka <pekka.lehtikoski@gmail.com>
2021-04-28 14:55:17 -0400

    xx

--- dependencies/eosal_jpeg/eosal_jpeg.h
+++ dependencies/eosal_jpeg/eosal_jpeg.h
@@ -33,7 +33,7 @@
  */
 #include "eosalx.h"
 
-/* If C++ compilation, all functions, etc. from this point on in included headers are
+/* If C++ compilation, all functions, etc. from this point on included headers are
    plain C and must be left undecorated.
  */
 OSAL_C_HEADER_BEGINS
