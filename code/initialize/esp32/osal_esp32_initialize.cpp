d632a849e82049459acb32c094cf1e1a4de4110d
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2021-09-03 10:20:15 -0400

    making esp-idf build

--- code/initialize/esp32/osal_esp32_initialize.cpp
+++ code/initialize/esp32/osal_esp32_initialize.cpp
@@ -1,12 +1,14 @@
 /**
 
-  @file    initialize/esp/osal_os_initialize.c
-  @brief   Operating system specific OSAL initialization.
+  @file    initialize/esp/osal_esp32_initialize.c
+  @brief   Operating system specific initialization.
   @author  Pekka Lehtikoski
   @version 1.0
-  @date    12.3.2020
+  @date    3.9.2021
 
-  Operating system specific OSAL initialization and shut down.
+  Operating system specific initialization for ESP-IDF sets log message levels, disables watchdog
+  timers and prints some HW information. This file also implements osal_reboot() function to 
+  restart the microcontroller.
 
   Copyright 2020 Pekka Lehtikoski. This file is part of the eosal and shall only be used,
   modified, and distributed under the terms of the project licensing. By continuing to use, modify,
@@ -22,9 +24,10 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_WARN
 
 #include "esp_log.h"
-#ifndef OSAL_ESPIDF_FRAMEWORK
 #include "esp_system.h"
-#endif
+#include "esp_spi_flash.h"
+#include "soc/rtc_wdt.h"
+
 
 /* Prototypes of forward referred static functions.
  */
@@ -39,19 +42,19 @@ static void osal_print_esp32_info(
   @brief Operating system specific OSAL library initialization.
   @anchor osal_init_os_specific
 
-  The osal_init_os_specific() function does operating system specific initialization
-  OSAL library for use.
-
+  The osal_init_os_specific() function for ESP32F sets log message levels, disables watchdog
+  timers and prints some HW information. 
+  
   @param  flags Bit fields. OSAL_INIT_DEFAULT (0) for normal initialization.
           OSAL_INIT_NO_LINUX_SIGNAL_INIT not to initialize linux signals.
-
-  @return  None.
 
 ****************************************************************************************************
 */
 void osal_init_os_specific(
     os_int flags)
 {
+    esp_chip_info_t chip_info;
+
     /* Set esp-idf event logging levels.
      */
     esp_log_level_set("*", ESP_LOG_WARN);
@@ -60,16 +63,38 @@ void osal_init_os_specific(
 
     /* No watchdog timers.
      */
+#ifdef OSAL_ESPIDF_FRAMEWORK
+    rtc_wdt_protect_off();
+    rtc_wdt_disable();
+#else
     disableLoopWDT();
     disableCore0WDT();
     disableCore1WDT();
+#endif    
+
+    /* Print some generic system information.
+     */
+    esp_chip_info(&chip_info);
+    osal_print_esp32_info("\nNro cores:   ", chip_info.cores);
+    osal_print_esp32_info("Silicon rev: ", chip_info.revision);
+    osal_console_write   ("WiFi:        ");
+    if (chip_info.features & CHIP_FEATURE_BT) {
+        osal_console_write("/BT");
+    }
+    if (chip_info.features & CHIP_FEATURE_BLE) {
+        osal_console_write("/BLE");
+    }
+    osal_console_write("\n");
+    osal_print_esp32_info("Silicon rev: ", chip_info.revision);
+    osal_print_esp32_info((chip_info.features & CHIP_FEATURE_EMB_FLASH) 
+      ? "Flash emb:"    : "Flash ext:   ", spi_flash_get_chip_size());
 
     /* Print amount of heap and PS ram
      */
-    osal_print_esp32_info("\nTotal heap: ", (os_long)ESP.getHeapSize());
-    osal_print_esp32_info("Free heap: ", (os_long)ESP.getFreeHeap());
-    osal_print_esp32_info("Total PSRAM:  ", (os_long)ESP.getPsramSize());
-    osal_print_esp32_info("Free PSRAM: ", (os_long)ESP.getFreePsram());
+    osal_print_esp32_info("Total heap:  ", (os_long)ESP.getHeapSize());
+    osal_print_esp32_info("Free heap:   ", (os_long)ESP.getFreeHeap());
+    osal_print_esp32_info("Total PSRAM: ", (os_long)ESP.getPsramSize());
+    osal_print_esp32_info("Free PSRAM:  ", (os_long)ESP.getFreePsram());
 }
 
 
@@ -79,11 +104,10 @@ void osal_init_os_specific(
   @brief Print ESP32 information to console.
   @anchor osal_print_esp32_info
 
-  The osal_print_esp32_info() function just prints label and value to console.
+  The osal_print_esp32_info() is a helper function to prints a label and value to console.
 
   @param  label Label text
   @param  value Value to print.
-  @return  None.
 
 ****************************************************************************************************
 */
@@ -103,12 +127,10 @@ static void osal_print_esp32_info(
 /**
 ****************************************************************************************************
 
-  @brief Shut down operating system specific part of the OSAL library.
+  @brief Shut down operating system.
   @anchor osal_shutdown
 
-  The osal_shutdown_os_specific() function...
-
-  @return  None.
+  Shutdown is not needed for ESP32.
 
 ****************************************************************************************************
 */
@@ -121,13 +143,10 @@ void osal_shutdown_os_specific(
 /**
 ****************************************************************************************************
 
-  @brief Reboot the computer.
+  @brief Restart the microcontroller.
   @anchor osal_reboot
 
-  The osal_reboot() function...
-
   @param   flags Reserved for future, set 0 for now.
-  @return  None.
 
 ****************************************************************************************************
 */
@@ -139,7 +158,6 @@ void osal_reboot(
 #endif
 
     os_sleep(200);
-
     esp_restart();
 }
 
